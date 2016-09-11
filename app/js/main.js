709645de40a236aff49adc345bd3fb5c751a46fc
helloberries <helloberriesqz@gmail.com>
2016-09-11 22:09:14 +0800

    use ajax method instead of promise

--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,55 @@
 $(() => {
   let $booksElem = $('#books'),
+      $pagesElem = $('#pages'),
       $searchInput = $('#search-input');
 
   let searchVal = '';
 
-  // Promise: 载入数据
-  let loadData = () => {
-    let promise = new Promise((resolve,reject) => {
+  // 载入数据
 
       $('form').submit((e) => {
         e.preventDefault();
+
+        // 清空搜索结果
+        $booksElem.html('');
+
+        // 获取搜索字段
         let searchVal = $searchInput.val();
         console.log(searchVal);
 
-        return $.ajax({
-              		type: 'GET',
-              		url: `https://api.douban.com/v2/book/search?q=${searchVal}&count=4`,
-              		dataType: 'jsonp',
-                  success(data) {
-                    resolve(data, searchVal);
-                  }
-              	});
+        $.ajax({
+        		type: 'GET',
+        		url: `https://api.douban.com/v2/book/search?q=${searchVal}&count=100`,
+        		dataType: 'jsonp',
+            success(data) {
+
+              let bookInfoArr = data.books;
+
+              // 分页
+              let pageSize = 4;
+              for (let i=1; i<(bookInfoArr.length/pageSize); i++) {
+                $pagesElem.append(
+                  `<li class="page fl">${i}</li>`
+                );
+              }
+
+              // 显示结果
+              for (let bookInfo of bookInfoArr) {
+                $booksElem.append(
+                  `<li class="book">
+                    <a href="${bookInfo.alt}">
+                      <img class="book-image" src="${bookInfo.image}">
+                      <p class="book-title">${bookInfo.title}</p>
+                      <p class="book-author">${bookInfo.author}</p>
+                      <p class="book-publisher">${bookInfo.publisher}</p>
+                    </a>
+                  </li>`);
+                }
+            }
+
+          });
       });
 
 
-    });
-
-    return promise;
-  };
-
-
-    loadData().then((data) => {
-      console.log('then',data);
-      console.log(searchVal);
-
-      let bookInfoArr = data.books;
-      for (let bookInfo of bookInfoArr) {
-        $booksElem.append(
-          `<li class="book">
-            <a href="${bookInfo.alt}">
-              <img class="book-image" src="${bookInfo.image}">
-              <p class="book-title">${bookInfo.title}</p>
-              <p class="book-publisher">${bookInfo.publisher}</p>
-            </a>
-          </li>`);
-      }
-    }).catch((error) => {
-      console.log('出错了',error);
-    });
 
 });
